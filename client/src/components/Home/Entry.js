0ef75f9a8488deb982ff724d85396f130d014e31
Marsela Sulku <msulku15@gmail.com>
2022-02-08 23:48:42 -0800

    Entry.js 4 spaces -> 2

--- client/src/components/Home/Entry.js
+++ client/src/components/Home/Entry.js
@@ -5,49 +5,50 @@ import TextInput from './TextInput';
 import './Entry.css';
 
 export default function Entry() {
-    return (
-        <div className="Entry">
-            <div className="Entry-buttons">
-                <div className="Entry-buttons-child" alt="save">
-                    <FaCheckCircle onClick={() => alert("save clicked")} className="Entry-buttons-child-icon" />
-                </div>
-                <div className="Entry-buttons-child">
-                    <FaPen onClick={() => alert("edit clicked")} className="Entry-buttons-child-icon" />
-                </div>
-                <div className="Entry-buttons-child">
-                    <FaTrash onClick={() => alert("delete clicked")} className="Entry-buttons-child-icon" />
-                </div>
-            </div>
-            <div className="Entry-body"> 
-                <div className="Entry-body-child" id="Entry-date">
-                    febuary 7, 2021
-                </div>
-                <div className="Entry-body-child Entry-questions-container">
-                    <div>
-                        <div className="Entry-question">
-                            what does your normal day look like?
-                        </div>
-
-                        <TextInput />
-                    </div>
-
-                    <div>
-                        <div className="Entry-question">
-                            what are you biggest goals right now? 
-                        </div>
-
-                        <TextInput />
-                    </div>
-
-                    <div>
-                        <div className="Entry-question">
-                            what are you watching nowadays?
-                        </div>
-                        
-                        <TextInput />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="Entry">
+     
+      <div className="Entry-buttons">
+        <div className="Entry-buttons-child" alt="save">
+          <FaCheckCircle onClick={() => alert("save clicked")} className="Entry-buttons-child-icon" />
         </div>
-    )
+     
+        <div className="Entry-buttons-child">
+          <FaPen onClick={() => alert("edit clicked")} className="Entry-buttons-child-icon" />
+        </div>
+     
+        <div className="Entry-buttons-child">
+          <FaTrash onClick={() => alert("delete clicked")} className="Entry-buttons-child-icon" />
+        </div>
+      </div>
+
+      <div className="Entry-body"> 
+        <div className="Entry-body-child" id="Entry-date">
+          febuary 7, 2021
+        </div>
+        <div className="Entry-body-child Entry-questions-container">
+          <div>
+            <div className="Entry-question">
+              what does your normal day look like?
+            </div>
+            <TextInput />
+          </div>
+
+          <div>
+            <div className="Entry-question">
+              what are you biggest goals right now? 
+            </div>
+            <TextInput />
+          </div>
+
+          <div>
+            <div className="Entry-question">
+              what are you watching nowadays?
+            </div>
+            <TextInput />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
